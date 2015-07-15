c495d19fee64fc050c8b02aff94b5280cdf91717
dustin-H <dustin.hoffner@gmail.com>
2015-07-15 20:41:43 +0200

    Fix Scope bug

--- es6src/render/hostfiles.js
+++ es6src/render/hostfiles.js
@@ -1,31 +1,35 @@
 var fs = require('fs');
 
+var read = function(req, res, next, file) {
+	fs.readFile(file.path, function(err, data) {
+		if (err) {
+			res.writeHead(404);
+			return res.end("File not found.");
+		}
+		var contentType;
+		switch (file.type) {
+			case 'js':
+				contentType = 'application/javascript';
+				break;
+			case 'css':
+				contentType = 'text/css';
+				break;
+			default:
+				contentType = 'text/plain';
+		}
+		res.setHeader("Content-Type", contentType);
+		res.writeHead(200);
+		res.end(data);
+	});
+}
+
 var hostFiles = function(req, res, next) {
 	var match = false;
 	if (req.unijs._path.substr(0, 4) === "/.f/") {
 		for (var i = 0, len = req.unijs.app._hostfiles.length; i < len; i++) {
 			if ('/.f/' + req.unijs.app._hostfiles[i]._id + '.' + req.unijs.app._hostfiles[i].type === req.unijs._path) {
 				match = true;
-				fs.readFile(req.unijs.app._hostfiles[i].path, function(err, data) {
-					if (err) {
-						res.writeHead(404);
-						return res.end("File not found.");
-					}
-					var contentType;
-					switch (req.unijs.app._hostfiles[i].type) {
-						case 'js':
-							contentType = 'application/javascript';
-							break;
-						case 'css':
-							contentType = 'text/css';
-							break;
-						default:
-							contentType = 'text/plain';
-					}
-					res.setHeader("Content-Type", contentType);
-					res.writeHead(200);
-					res.end(data);
-				});
+				read(req, res, next, req.unijs.app._hostfiles[i]);
 			}
 		}
 	}
