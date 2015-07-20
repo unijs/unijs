0d346343ee9b49c984fa3745b22c708d6465bd8a
dustin-H <dustin.hoffner@gmail.com>
2015-07-20 23:11:15 +0200

    Include renderCache to clientapi

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var requireNodeJsOnly = require;
 
 var checkLocation = require('./build/utils/checkLocation.js');
+var cache = require('./render/cache.js');
 
 var m = {};
-
 
 m.checkLocation = checkLocation;
 m.superagentPlugin = require('./build/exports/superagentPlugin.js');
 m.extend = require('./build/exports/extend.js');
-m.Server = function(){
+m.render = {
+	cache: cache
+};
+m.Server = function() {
 	checkLocation.setServer();
 	return requireNodeJsOnly('./build/Server.js');
 };
