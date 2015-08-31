30cc14154e0665591a6e7560e6f53225488854d4
dustin-H <dustin.hoffner@gmail.com>
2015-08-31 21:37:47 +0200

    Fixed #11

--- es6src/render/hostfiles.js
+++ es6src/render/hostfiles.js
@@ -26,18 +26,25 @@ var read = function(req, res, next, file) {
 	});
 }
 
-var hostFiles = function(req, res, next) {
+var hostFiles = function hostFiles(req, res, next) {
 	var match = false;
-	console.log('path', req.path, req._path);
-	if (req.unijs._path.substr(0, 4) === "/.f/") {
+	var split = req.path.split('/');
+	var file = split.pop();
+	var folder = split.pop();
+	if (folder === '.f') {
 		for (var i = 0, len = req.unijs.app._hostfiles.length; i < len; i++) {
-			if ('/.f/' + req.unijs.app._hostfiles[i]._id + '.' + req.unijs.app._hostfiles[i].type === req.unijs._path) {
+			var arr = file.split('.');
+			if (req.unijs.app._hostfiles[i]._id === arr[0] && req.unijs.app._hostfiles[i].type === arr[1]) {
 				match = true;
 				read(req, res, next, req.unijs.app._hostfiles[i]);
+				return true;
 			}
 		}
+		res.writeHead(404);
+		res.end('File not found.');
+		return true;
 	}
-	return match;
-}
+	return false;
+};
 
 module.exports = hostFiles;
